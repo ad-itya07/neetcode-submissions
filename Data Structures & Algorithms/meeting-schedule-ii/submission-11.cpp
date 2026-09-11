7071d01d2acb9e956b70d74e22d1ccbfed3cbfec
Aditya Jha <141293751+ad-itya07@users.noreply.github.com>
2026-09-11 12:36:08 +0530

    Add: meeting-schedule-ii - submission-11

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-11.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+    static bool cmp(Interval& a, Interval& b) {
+        return a.start < b.start;
+    }
+    int minMeetingRooms(vector<Interval>& ints) {
+        int n = ints.size();
+
+        sort(ints.begin(), ints.end(), cmp);
+        priority_queue<int, vector<int>, greater<int>> pq;
+
+        int res=0;
+        for (int i=0; i<n; i++) {
+            if (pq.empty()) pq.push(ints[i].end);
+            if (!pq.empty()) {
+                if (pq.top() > ints[i].start) {
+                    res++;
+                    pq.push(ints[i].end);
+                }
+                else {
+                    while (!pq.empty() && pq.top() < ints[i].start) pq.pop();
+                    pq.push(ints[i].end);
+                }
+            }
+        }
+
+        return res;
+    }
+};
